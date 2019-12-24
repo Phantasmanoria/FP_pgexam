ae0a6b36083081d29f5552021e1306be14fb4d48
phantasmanoria <phantasmanoria@gmail.com>
2019-12-25 01:28:19 +0900

    fix host sort in save memory mode

--- lib/save.rb
+++ lib/save.rb
@@ -38,14 +38,15 @@ class Analysis_Save < Analysis
   def hour(in_files) # 時間別のデータ分析
     Display.top("HOUR")
     ana_files = count(in_files,"HOUR")
-    unify_sort(ana_files)
+    ana_files = unify_sort(ana_files, "HOUR")
   end
 
   
   def host(in_files) # ホスト別のデータ分析
     Display.top("HOST")
     ana_files = count(in_files,"HOST")
-    unify_sort(ana_files)
+    ana_files = unify_sort(ana_files, "HOST", true)
+    ana_files = unify_sort(ana_files, "HOST")
   end
 
 
@@ -80,24 +81,34 @@ class Analysis_Save < Analysis
     w_files
   end
 
-  def unify_sort(ana_files) # 分割された分析済ファイルをメモリ考慮しつつ出力
 
-    mode = ana_files[0][/hour/] unless ana_files[0][/hour/].nil? # 後のモード判別
-    mode = ana_files[0][/host/] unless ana_files[0][/host/].nil?
+  def unify_sort(ana_files, mode, tmp=false) # 分割された分析済ファイルをメモリ考慮しつつ出力
+    mode.downcase! # モード名の小文字化
 
     candidate = [] # 出力する候補
     top = "" # 出力する文字列
     n = 0 # 出力する値
     files_line = [] # 各ファイルの出力行
-    for a in 1..ana_files.size do
+
+    c = 0 # tmp用カウント
+    w_files = [] # tmp用出力ファイル
+    tmp_result = {} # tmp用関数保存
+    # ここでのtmpは表示をお預けするか否(すぐ表示)か, HOSTが二回回しが必要だと判断した為差別化した
+    if tmp 
+      name = "tmp/ana_tmp_#{w_files.length+1}.tmp"
+      w_files.push(name)
+      w_file = eval("File.open('#{name}', 'w')")
+    end
+    
+    for a in 1..ana_files.size do # 読み込み行の初期化
       files_line.push(0)
     end
 
     loop {
-      for a in 0..ana_files.size-1 do # 各ファイルの候補読み取り(下限に来てたらdummy挿入)
+      for a in 0..ana_files.size-1 do # 各ファイルの候補読み取り(読み込み下限に来てたらdummy挿入)
         d = Convert.read_line(ana_files[a],files_line[a])
         unless d.nil?
-          candidate.push([d[0], d[1]])
+          candidate.push([d[0], d[1].to_i])
         else
           candidate.push(["z","dummy"])
         end
@@ -105,21 +116,49 @@ class Analysis_Save < Analysis
 
       break if check(candidate) # 候補が残ってないかチェック
 
-      top = (candidate.sort_by {|x| x[0]})[0][0] # 出力すべき文字列を発見
-
+      if tmp
+        top = (candidate.sort_by {|x| -x[1]})[0][0] # 出力すべき文字列を発見(アクセス降順)
+      else
+        top = (candidate.sort_by {|x| x[0]})[0][0] # 出力すべき文字列を発見(値昇順)
+      end
+      
       for a in 0..candidate.size-1 do # 複数に同じ文字列がある時の処理
         if candidate[a][0] == top 
           n += candidate[a][1].to_i # アクセス数の統合
           files_line[a] += 1 # 該当したファイルの読み込み行を1つずらす
         end
       end
-
-      eval("Display.#{mode}([[top, n]])") # 表示を渡す
       
-      candidate = [] # 初期化
+      if tmp # tmp時, 値保存
+        eval("tmp_result[:#{top}] = #{n.to_i}") 
+        c += 1
+        if c == 100000 # 一定毎に出力ファイルを分割(分割後の読み取りメモリ対策)(規定10万)
+          for d in tmp_result.sort_by{ |_, v| -v } do
+            w_file.puts("#{d[0]} #{d[1]}")
+          end
+          w_file.close
+          name = "tmp/ana_tmp_#{w_files.length+1}.tmp"
+          w_files.push(name)
+          w_file = eval("File.open('#{name}', 'w+')")
+          c = 0 # 変数リセット
+          tmp_result = {}
+        end
+      else
+        eval("Display.#{mode}([[top, n]])") # tmp時ではないなら表示をそのまま渡す
+      end
+
+      candidate = [] # 変数リセット
       n = 0
     }
-    
+
+    if tmp # loop終了時もtmp時は溜まっているresultを出力
+      for d in tmp_result.sort_by{ |_, v| -v } do
+        w_file.puts("#{d[0]} #{d[1]}")
+      end
+        w_file.close
+    end
+
+    w_files # tmp時のファイルリストを出力(そうでないなら空)
   end
 
   
